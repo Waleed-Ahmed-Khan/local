e066b4792755b268b81039898dc5aab1ca883dff
Waleed-Ahmed-Khan <waleedahmed20997@gmail.com>
2024-03-11 14:14:35 +0500

    Update setup

--- setup.py
+++ setup.py
@@ -37,11 +37,11 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
     install_requires = [
-        "plotly >= 5.8.0",
-        "numpy >= 1.22.4",
-        "pandas >= 1.3.5",
-        "seaborn >= 0.11.1",
-        "scikit-learn==1.1.2",
-        "kaleido==0.0.3"
+        "plotly >= 5.19.0",
+        "numpy >= 1.26.4",
+        "pandas >= 2.2.1",
+        "seaborn >= 0.13.w",
+        "scikit-learn==1.4.0",
+        "kaleido==0.2.1"
     ]
 )   
